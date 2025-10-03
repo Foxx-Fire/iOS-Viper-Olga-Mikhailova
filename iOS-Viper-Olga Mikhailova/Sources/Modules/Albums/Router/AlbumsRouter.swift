42d660fb845b66e3baa824124bf21991a9dbc15d
Olga Mikhailova <foxx_fire@mail.ru>
2025-10-03 22:08:03 +0300

    Create router for Albums

--- iOS-Viper-Olga Mikhailova/Sources/Modules/Albums/Router/AlbumsRouter.swift
+++ iOS-Viper-Olga Mikhailova/Sources/Modules/Albums/Router/AlbumsRouter.swift
@@ -5,3 +5,29 @@
 //  Created by FoxxFire on 30.09.2025.
 //
 
+import UIKit
+
+final class AlbumsRouter {
+    
+    // MARK: - VIPER Connections
+    weak var viewController: UIViewController?
+}
+
+// MARK: - AlbumsRouterInput
+extension AlbumsRouter: AlbumsRouterInput {
+    
+    func showAlbumDetail(_ album: CellItemProtocol) {
+        // Здесь будет создание и показ экрана деталей альбома
+        print("Navigating to album detail: \(album.itemId)")
+    }
+    
+    func showError(_ error: any Error) {
+        let alert = UIAlertController(
+            title: "Ошибка",
+            message: error.localizedDescription,
+            preferredStyle: .alert
+        )
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        viewController?.present(alert, animated: true)
+    }
+}
